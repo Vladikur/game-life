ad5d2933693eb41ef22a4ae342e48588d22034f6
Vlad Kurmaev <Vladikurmav@yandex.ru>
2023-02-28 23:02:48 +0500

    feat: add styles and locomotive

--- src/js/Figures.js
+++ src/js/Figures.js
@@ -11,6 +11,9 @@ export default class Figures {
             case 'glider':
                 fieldWithFigure = this.createGlider()
                 break;
+            case 'locomotive':
+                fieldWithFigure = this.createLocomotive()
+                break;
         }
 
         return fieldWithFigure
@@ -36,6 +39,39 @@ export default class Figures {
         figure[39][37] = 1
         figure[40][37] = 1
         figure[38][36] = 1
+
+        return figure
+    }
+
+    createLocomotive() {
+        const figure = getField()
+
+        figure[39][38] = 1
+        figure[39][37] = 1
+        figure[40][37] = 1
+        figure[38][36] = 1
+        figure[38][37] = 1
+        figure[39][36] = 1
+        figure[40][36] = 1
+        figure[40][40] = 1
+        figure[42][40] = 1
+        figure[38][40] = 1
+        figure[36][40] = 1
+        figure[35][41] = 1
+        figure[35][42] = 1
+        figure[35][43] = 1
+        figure[35][44] = 1
+        figure[43][41] = 1
+        figure[43][42] = 1
+        figure[43][43] = 1
+        figure[43][44] = 1
+        figure[42][44] = 1
+        figure[41][44] = 1
+        figure[35][44] = 1
+        figure[36][44] = 1
+        figure[37][44] = 1
+        figure[38][43] = 1
+        figure[40][43] = 1
 
         return figure
     }
